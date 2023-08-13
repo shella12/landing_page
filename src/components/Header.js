e4085fb5d0c11b4e8abe173a6989113fcaaedb47
shella12 <ayeshaarshad4567@gmail.com>
2023-08-13 12:49:10 +0500

    Add boiler

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <nav className='flex'>
+        <a href='#'>sign in</a>
+        <a href='#'>legal</a>
+        <a href='#'>licenses</a>
+        <a href='#'>security</a>
+        <a href='#'>careers</a>
+        <a href='#'>press</a>
+        <a href='#'>support</a>
+        <a href='#'>status</a>
+        <a href='#'>codeblog</a>
+    </nav>
+  )
+}
+
+export default Header
